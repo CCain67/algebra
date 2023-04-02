7e6951430ebfd03805626f11af1ce543a7ad6e3f
ccain67 <charlescain0607@gmail.com>
2023-04-01 23:19:00 -0400

    added more unit tests, restructured unit test folder

---
+++ tests/groups/test_orders.py
@@ -0,0 +1,35 @@
+import pytest
+
+from sympy import totient
+
+from group_theory.common_groups.residue_class_groups import *
+from group_theory.common_groups.permutation_groups import *
+from group_theory.common_groups.matrix_groups import *
+from group_theory.common_groups.misc_groups import *
+
+def test_cyclic_group_order():
+    assert CyclicGroup.as_residue_class_group(31).order == 31
+
+def test_multiplicative_residue_class_group_order():
+    assert GroupOfUnits.as_residue_class_group(16).order == totient(16)
+
+def test_symmetric_group_order():
+    assert SymmetricGroup.as_permutation_group(4).order == 24
+    assert SymmetricGroup.as_matrix_group(4).order == 24
+
+def test_dihedral_group():
+    assert DihedralGroup.as_permutation_group(7).order == 14
+
+def get_gl_n_p_order(n,p):
+    k = 0
+    prod = 1 
+    while k<=n-1:
+        prod *= (p**n-p**k)
+        k += 1
+    return prod
+
+def test_gl_n_p_order():
+    assert GeneralLinear.over_finite_field(2,3).order == get_gl_n_p_order(2,3)
+
+def test_sl_n_p_order():
+    assert SpecialLinear.over_finite_field(2,3).order == get_gl_n_p_order(2,3)/2
--- tests/order/test_orders.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from group_theory.common_groups.residue_class_groups import *
-from group_theory.common_groups.permutation_groups import *
-from group_theory.common_groups.matrix_groups import *
-from group_theory.common_groups.misc_groups import *
-
-def test_cyclic_group_order():
-    assert CyclicGroup.as_residue_class_group(29).order == 29
