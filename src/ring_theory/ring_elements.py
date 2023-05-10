3382c9b5d84bbe5ace2149138aa3fb158a3cb57c
ccain67 <charlescain0607@gmail.com>
2023-05-09 22:10:32 -0400

    start of the ring_theory module - RingElements and Rings

---
+++ src/ring_theory/ring_elements.py
@@ -0,0 +1,147 @@
+"""This module defines several various classes of ring elements."""
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from functools import reduce
+
+import galois
+
+from group_theory.group_elements import GroupElement
+
+
+class RingElement(ABC):
+    """Base class representing elements of arbitrary finite rings."""
+
+    @abstractmethod
+    def additive_order(self):
+        """abstract method for fetching the additive order of the element."""
+
+    @abstractmethod
+    def multiplicative_order(self):
+        """abstract method for fetching the additive order of the element."""
+
+    @abstractmethod
+    def is_additive_identity(self):
+        """abstract method for determining whether or not the RingElement is
+        the additive identity element of the ring.
+        """
+
+    @abstractmethod
+    def is_multiplicative_identity(self):
+        """abstract method for determining whether or not the RingElement is
+        the multiplicative identity element of the ring.
+        """
+
+    @abstractmethod
+    def is_invertible(self):
+        """abstract method for determining whether or not the RingElement
+        has a multiplicative inverse."""
+
+
+class GroupElementAdapter(GroupElement):
+    """An adapter which converts a RingElement to a GroupElement"""
+
+    def __init__(self, ring_element: RingElement) -> None:
+        self.ring_element = ring_element
+        for attribute in ring_element.__dict__:
+            setattr(self, attribute, ring_element.__dict__[attribute])
+
+    def __repr__(self) -> str:
+        return repr(self.ring_element)
+
+    def __hash__(self) -> int:
+        return hash(self.ring_element)
+
+    def __mul__(self, other) -> GroupElementAdapter:
+        return GroupElementAdapter(self.ring_element * other.ring_element)
+
+    def __eq__(self, other):
+        return self.ring_element == other.ring_element
+
+    def __ne__(self, other):
+        return self.ring_element != other.ring_element
+
+    def get_order(self):
+        return self.ring_element.multiplicative_order()
+
+    def is_identity(self):
+        return self.ring_element.is_multiplicative_identity()
+
+
+class RingResidueClass(RingElement):
+    """Base class for elements of the ring of integers modulo N."""
+
+    def __init__(self, residue: int, modulus: int):
+        self.modulus = modulus
+        self.residue = residue % modulus
+
+    def __repr__(self):
+        return "[" + str(self.residue) + "]"
+
+    def __hash__(self):
+        return hash((self.residue, self.modulus))
+
+    def __eq__(self, other):
+        return (self.residue == other.residue) and (self.modulus == other.modulus)
+
+    def __ne__(self, other):
+        return not (self.residue == other.residue) or not (
+            self.modulus == other.modulus
+        )
+
+    def __add__(self, other):
+        if self.modulus != other.modulus:
+            raise ValueError("the moduli must be equal")
+        return RingResidueClass(
+            (self.residue + other.residue) % self.modulus, self.modulus
+        )
+
+    def __mul__(self, other):
+        if self.modulus != other.modulus:
+            raise ValueError("the moduli must be equal")
+        return RingResidueClass(
+            (self.residue * other.residue) % self.modulus, self.modulus
+        )
+
+    def __pow__(self, N: int):
+        if N > 0:
+            return reduce(lambda x, y: x * y, [self] * N)
+        if N < 0:
+            return reduce(lambda x, y: x * y, [~self] * abs(N))
+        return RingResidueClass(1, self.modulus)
+
+    def __neg__(self):
+        return RingResidueClass(-self.residue, self.modulus)
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __invert__(self):
+        if galois.gcd(self.residue, self.modulus) != 1:
+            raise ValueError(
+                """the residue and modulus must be relatively prime 
+                for a multiplicative inverse to exist"""
+            )
+        # this is Euler's theorem
+        exp = galois.euler_phi(self.modulus) - 1
+        return RingResidueClass(self.residue**exp, self.modulus)
+
+    def additive_order(self):
+        return int(self.modulus / galois.gcd(self.residue, self.modulus))
+
+    def multiplicative_order(self):
+        prod = self
+        i = 1
+        while not prod.is_multiplicative_identity():
+            prod *= self
+            i += 1
+        return i
+
+    def is_additive_identity(self) -> bool:
+        return self.residue == 0
+
+    def is_multiplicative_identity(self) -> bool:
+        return self.residue == 1
+
+    def is_invertible(self) -> bool:
+        return galois.gcd(self.residue, self.modulus) == 1
---
+++ src/ring_theory/rings.py
@@ -0,0 +1,96 @@
+"""
+This module defines the Ring, Subring, and Ideal classes. 
+"""
+
+from __future__ import annotations
+
+from group_theory.groups import Group
+from ring_theory.ring_elements import (
+    GroupElementAdapter,
+    RingElement,
+)
+
+
+class Ring:
+    """Base class representing a finite ring.
+
+    Args:
+        elements (list[RingElement]): a list of elements for the ring.
+    """
+
+    # pylint: disable=too-many-instance-attributes
+    # pylint: disable=too-many-public-methods
+
+    def __init__(self, elements: list[RingElement]):
+        self.elements = elements
+        self.order = len(self.elements)
+        self.additive_identity = self.get_additive_identity()
+        self.multiplicative_identity = self.get_multiplicative_identity()
+
+    def __repr__(self) -> str:
+        repr_string = ""
+        for ring_element in self:
+            repr_string += repr(ring_element) + "\n"
+        return repr_string
+
+    def __eq__(self, other) -> bool:
+        return set(self.elements) == set(other.elements)
+
+    def __ne__(self, other) -> bool:
+        return set(self.elements) != set(other.elements)
+
+    def __lt__(self, other) -> bool:
+        return set(self.elements).issubset(set(other.elements))
+
+    def __le__(self, other) -> bool:
+        return set(self.elements).issubset(set(other.elements))
+
+    def __gt__(self, other) -> bool:
+        return set(self.elements).issuperset(set(other.elements))
+
+    def __ge__(self, other) -> bool:
+        return set(self.elements).issuperset(set(other.elements))
+
+    def __hash__(self) -> int:
+        return hash(frozenset(self.elements))
+
+    def __getitem__(self, key) -> RingElement:
+        return self.elements[key]
+
+    def __iter__(self):
+        return iter(self.elements)
+
+    def __len__(self) -> int:
+        return len(self.elements)
+
+    def get_additive_identity(self) -> RingElement:
+        """Fetches the additive identity element of the ring
+
+        Returns:
+            RingElement: the additive identity element of the ring.
+        """
+        for element in self.elements:
+            if element.is_additive_identity():
+                self.identity = element
+                return element
+        return None
+
+    def get_multiplicative_identity(self) -> RingElement:
+        """Fetches the multiplicative identity element of the ring
+
+        Returns:
+            RingElement: the multiplicative identity element of the ring.
+        """
+        for element in self.elements:
+            if element.is_multiplicative_identity():
+                self.identity = element
+                return element
+        return None
+
+    def group_of_units(self) -> Group:
+        """Computes the group of units of the ring.
+
+        Returns:
+            Group: the group of invertible elements of the ring.
+        """
+        return Group([GroupElementAdapter(r) for r in self if r.is_invertible()])
